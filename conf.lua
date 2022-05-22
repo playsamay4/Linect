1bb8009b36ed311f18b8d0ba07c2ed5553da9025
playsamay4 <playsamay4>
2022-05-21 21:54:54 -0700

    changed name from "Linnect" to "Linect"

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 function love.conf(t)
     t.window.width = 1280
     t.window.height = 720
-    t.window.title = "Linnect"
+    t.window.title = "Linect"
     t.window.fullscreen = false
     t.console = true
 end
